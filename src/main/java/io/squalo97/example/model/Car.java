d469df5abb255bdb9f7302f07f2bf37548d12683
Squalo97 <gava.alberto97@gmail.com>
2023-11-28 19:56:34 +0100

    Car created

---
+++ src/main/java/io/squalo97/example/model/Car.java
@@ -0,0 +1,10 @@
+package io.squalo97.example.model;
+
+public class Car {
+    byte wheels = 4;
+    short horsepower = 250;
+    float weight = 2_000;
+    FuelEnum fuel = FuelEnum.GASOLINE;
+    boolean leatherIntern = true;
+    String paint = "Blue";
+}
---
+++ src/main/java/io/squalo97/example/model/FuelEnum.java
@@ -0,0 +1,7 @@
+package io.squalo97.example.model;
+
+public enum FuelEnum {
+    GASOLINE,
+    DIESEL,
+    ELECTRIC;
+}
